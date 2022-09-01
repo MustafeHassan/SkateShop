b7f94477f32ae3052c571ee193de7d72e83cf89c
xbanker3 <72931678+xbanker3@users.noreply.github.com>
2022-09-01 17:38:23 +0300

    Update

--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -3,9 +3,7 @@ import 'dart:async';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:lottie/lottie.dart' as lottie ;
 import 'package:shimmer/shimmer.dart';
-
 import '../models/locations.dart';
 import '../utils/styles.dart';
 
